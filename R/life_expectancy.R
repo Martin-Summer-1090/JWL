97a7626f2e1d392c900d678533cfaa5064f0cf85
Martin Summer <martin.summer@chello.at>
2023-10-15 22:49:04 +0200

    added a dataset on life expectancy and gdp per capita from our world in data

---
+++ R/life_expectancy.R
@@ -0,0 +1,15 @@
+#' Life expectancy and GDP per capita in 2011 international dollars.
+#'
+#' Data on life expectancy and GDP per capita in 2011 international dollars
+#' for countries around the globe from 1950 -2018
+#'
+#' @format A data frame with 12447 rows and 5 variables:
+#' \describe{
+#'   \item{Entity}{Country name, chr}
+#'   \item{Code}{ISO3 code of country, chr}
+#'   \item{Year}{Year, num, year of observation}
+#'   \item{LE}{Life expectancy at birth, num}
+#'   \item{GDPC}{GDP per capita in 2011 international dollars}
+#' }
+#' @source \url{https://ourworldindata.org/life-expectancy}
+"life_expectancy"
