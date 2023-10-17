1dd8bd5791f008306730e69cd42eaea72b3b8878
Martin Summer <martin.summer@chello.at>
2023-10-17 21:26:23 +0200

    Historical data of Pearson experiment on height of fathers and sons from 1903 added

---
+++ R/pearson_father_son.R
@@ -0,0 +1,14 @@
+#' Historical data of Pearson 1903 of heights data of fathers and sons.
+#'
+#' Heights of fathers and their sons in inches, based on a famous experiment by Karl Pearson
+#' around 1903. The number of cases is 1078.
+#' Random noise was added to the original data, to produce heights to the nearest 0.1 inch.
+#'
+#' @format A data frame with 1078 rows and 2 variables:
+#' \describe{
+#'   \item{Father}{Height of father in inches, num}
+#'   \item{Son}{Height of son in inches, num}
+#' }
+#' @source Department of Statistics, University of California, Berkeley
+#' \url{https://www.randomservices.org/random/data/Pearson.html}
+"pearson"
