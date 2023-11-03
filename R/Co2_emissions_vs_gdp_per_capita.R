915abbe224a7dff3fbd3ef4c41e2b87e3492827a
Martin Summer <martin.summer@chello.at>
2023-11-03 23:52:57 +0100

    new dataset on co2 emissions per capita versus gdp per capita added

---
+++ R/Co2_emissions_vs_gdp_per_capita.R
@@ -0,0 +1,15 @@
+#' co2_emissions_versus_gdp_per_capita
+#'
+#' Data on co2 emissions per capita versus gdp per capita from 1950 to 1918
+#' for countries around the globe
+#'
+#' @format A data frame with 13769 rows and 5 variables:
+#' \describe{
+#'   \item{Entity}{Country name, chr}
+#'   \item{Code}{ISO3 code of country, chr}
+#'   \item{Year}{Year, num, year of observation}
+#'   \item{CO2pc}{Co2 emissions per capita in a year, num}
+#'   \item{GDPpc}{GDP per capita in a year, num}
+#' }
+#' @source \url{https://ourworldindata.org/}
+"emissions"
