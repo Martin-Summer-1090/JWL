7a6bdd79bc2882bb5a02f0724d0da9eb8ad4fca7
Martin Summer <martin.summer@chello.at>
2023-12-11 23:02:33 +0100

    added dataset on advertising channels and sales

---
+++ R/advertising.R
@@ -0,0 +1,18 @@
+#' Data on different advertising channels and sales
+#'
+#' This is an example dataset from the literature on in statistical learning. It is
+#' analyzed in a book called Introduction to Statistical Learning by James, Witten, Hastie, and Tibshirani.
+#' The dataset consists of 200 samples with four variables: Advertising budgets  for newspaper,
+#' TV, and radio (the predictors) Sales (the outcome)
+#'
+#' @format A data frame with 200 rows and 5 variables:
+#' \describe{
+#'   \item{ID}{id of observation, num}
+#'   \item{TV}{Expenditures on TV advertising, num}
+#'   \item{radio}{Expenditures on radio advertising, num}
+#'   \item{newspaper}{Expenditures on newspaper advertising, num}
+#'   \item{sales}{Sales income, num}
+#'
+#' }
+#' @source https://www.statlearning.com/resources-second-edition
+"advertising"
