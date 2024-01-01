7d6abd92bd8a69ec44645d35e13aeb76ab76d6df
Martin Summer <martin.summer@chello.at>
2024-01-01 22:16:38 +0100

    added old faithful geysir erruption time data

---
+++ R/geysir_erruption_times.R
@@ -0,0 +1,16 @@
+#' Data on eruption times of the Geysir old faithful in Yellowstone
+#' National Park, Wyoming USA
+#'
+#' This is a famous dataset on the erruption times of the Geysir
+#' Old Faithful in Yellowstone National Park US. It is part of the R packages
+#' of datasets where it is stored under the name faithful. We us a version from
+#' the book of https://github.com/awstringer1/sta238-book/tree/master documenting
+#' erruption times in minutes only
+#'
+#' @format A data frame with 272 rows and 1 variables:
+#' \describe{
+#'   \item{time}{erruption time in minutes, num}
+#'
+#' }
+#' @source https://github.com/awstringer1/sta238-book/tree/master
+"oldfaithful"
