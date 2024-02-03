40abb46685116c5289dcde3b5c1412f90a6229a1
Martin Summer <martin.summer@chello.at>
2024-02-03 12:16:24 +0100

    gehlbach data added

---
+++ R/gehlbach.R
@@ -0,0 +1,19 @@
+#' Data from the evidence of Stephen Gehlbach in United States versus Kristen Gilbert
+#'
+#' These are data from hospital records at the Veteran's Administration (VA)
+#' hospital in Northhampton Massachusetts which were used in a court case
+#' United States versus Kristen Gilbert by Stephen Gehlbach, University of
+#' Massachusetts, School of Public Health and Health Sciences. The data report
+#' the number of deaths per year and shift in the hospital.
+#'
+#' @format A data frame with 30 rows and 5 variables:
+#' \describe{
+#'   \item{year}{year, num}
+#'   \item{shift}{shift, fct}
+#'   \item{start}{start of shift, date}
+#'   \item{end}{end of shift, date}
+#'   \item{number_of_deaths}{number of deaths, num}
+#'
+#' }
+#' @source https://books.google.at/books?id=28dQNQAACAAJ
+"gehlbach"
